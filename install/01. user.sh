be447c9cd6d3706745891c9e3cdc883024b0d6fa
k1ck3r <32503925+k1ck3r@users.noreply.github.com>
2017-11-07 12:00:43 +0200

    Update and rename 03. user_sys.sh to 01. user.sh

---
+++ install/01. user.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# adding user and configure the systemd requirements
+
+combatuser=$1
+
+if test -z "${combatuser}" ; then
+  printf "Combat Log username and systemd configuration\n"
+  printf "Usage: $0 <user-name>\n"
+  exit 1
+fi
+
+printf "Creating $combatuser user\n"
+if command -v useradd >/dev/null 2>&1 ; then
+  useradd -m -d /var/lib/$combatuser \
+    -r -s /usr/sbin/nologin $combatuser || true
+elif command -v adduser >/dev/null 2>&1 ; then
+  adduser -h /var/lib/$combatuser \
+    -s /sbin/nologin \
+    -S -D $1 || true
+else
+  printf "Unable to add $combatuser user\n"
+  exit 1
+fi
